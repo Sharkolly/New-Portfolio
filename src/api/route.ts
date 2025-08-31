a3b679aeb9c34d04af863d2e584a052132343a8e
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-08-31 18:59:33 +0100

    feat: working contact form

--- src/api/route.ts
+++ src/api/route.ts
@@ -6,7 +6,7 @@ export const send_message = async (message_details: {
   message: string;
 }) => {
   try {
-    const response = await axios.post("http://localhost:4000/api/contact", message_details);
+    const response = await axios.post("https://portfolio-server-ugdf.onrender.com/api/contact", message_details);
     return response.data;
   } catch (error) {
     const axios_error = error as AxiosError;
